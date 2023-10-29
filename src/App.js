6897205e9be152e7ad26adace14cfac58ba21899
Barbara Zhong <xbarbarazhong@gmail.com>
2023-10-29 15:27:58 -0700

    Add working images with hover

--- src/App.js
+++ src/App.js
@@ -7,39 +7,9 @@ function App() {
     return(
       <div className='App'>
         <BrandNavBar className="App-header"/>
-        <Images />
+        <Images/>
       </div>
     )
-    // <div className='App'>
-
-    //   // TODO: make a for loop to loop through assets
-    //   <div className='img_container'>
-    //     <img src={img1} className="App-img" alt="img1" />
-    //   </div>
-    //   <div className='img_container'>
-    //     <img src={img2} className="App-img" alt="img1" />
-    //   </div>
-    //   <div className='img_container'>
-    //     <img src={img3} className="App-img" alt="img1" />
-    //   </div>
-    // </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  
 }
 
 export default App;
--- src/Images.js
+++ src/Images.js
@@ -1,20 +1,62 @@
-import './App.css';
+import './Images.css';
 import React from 'react';
-var listOfImages =[];
+// var listOfImages =[];
 
-class Images extends React.Component{
+export const videos = [{
+  id: 0,
+  name: 'JAZZ & COOKING | in film',
+  summary: 'I love cooking. I love jazz. Let\'s see what happens when we combine them..',
+  youtube_link: 'https://www.youtube.com/watch?v=Cutzq2vX5HM&t=1s',
+  imageId: 'in_film_cooking_and_jazz.jpeg'
+}, {
+  id: 1,
+  name: 'Robert Eggers • OEUVRE',
+  summary: 'We are starting out strong with Robert Eggers, who in my opinion has one of the strongest visual and phonetic language working today.',
+  youtube_link: 'https://www.youtube.com/watch?v=JgPJNBo-Llg&t=98s',
+  imageId: 'robbert_eggers.jpeg'
+}, {
+  id: 2,
+  name: 'Vacation (Aftersun)',
+  summary: 'An homage to my favorite film of 2022. A beautiful, tender look at a father and daughter on vacation.',
+  youtube_link: 'https://www.youtube.com/watch?v=mZGEVzyAyNs&t=14s',
+  imageId: 'vacation.jpeg'
+}, 
+];
+
+function Image(props) {
+  return (
+    <div className='img_container'>  
+      <img className="image" key={props.index} src={props.image} alt="info"></img> 
+      <div className='middle'> 
+        <div className='text'>{props.name}</div>
+        <div className='text'>{props.summary}</div>
+      </div> 
+    </div> 
+  );
+}
+
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images;
+}
+
+const images = importAll(require.context('./assets/', false, /\.(png|jpe?g|svg)$/));
+
+class Images extends React.Component {
   importAll(r) {
     return r.keys().map(r);
-}
-componentWillMount() {
-  listOfImages = this.importAll(require.context('./assets/', false, /\.(png|jpe?g|svg)$/));
-}
-  render(){
+  }
+  // componentWillMount() {
+  //   listOfImages = this.importAll(require.context('./assets/', false, /\.(png|jpe?g|svg)$/));
+  // }
+  
+  render() {
     return(
-      <div>
+      <div className='full_images'>
         {
-          listOfImages.map(
-            (image, index) =>  <div className='img_container'>  <img className="App-img" key={index} src={image} alt="info"></img> </div>
+          videos.map(
+            (video) =>  <Image index={video.id} image={images[video.imageId]} summary={video.summary} name={video.name} />
           )
         }
       </div>
